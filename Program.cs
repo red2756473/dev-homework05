bd656d620b9e57c40dd746ec92b668dca5c8cc1e
red2756473 <85526605+red2756473@users.noreply.github.com>
2021-08-11 08:54:03 -0500

    los conejos

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace dev_homework05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int conejos = 15;
+            for (int o = 0; o < conejos; o ++){
+                Console.WriteLine("🐰");
+                for (int x = 0; x < o; x ++){
+                    Console.Write("🐰");
+                }
+            
+        
+
+           
+
+        }
+    }
+}}
+
